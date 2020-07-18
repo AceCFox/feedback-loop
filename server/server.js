a13c70efe9c0d8a456dce266055bd4449afdcfac
Ace Fox <afox09@gmail.com>
2020-07-18 13:39:13 -0500

    set up GET and POST route and postObject in review.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,30 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
+app.get('/feedback', (req, res) => {
+    console.log('Getting all feedback');
+    const queryString = `SELECT * FROM "feedback"`;
+    pool.query(queryString).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('Error GET /feedback', error);
+        res.sendStatus(500);  
+    });//end query
+})//end get
+
+app.post('/feedback', (req,res)=>{
+    console.log('posting new feedback', req.body)
+    const queryString = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`
+    pool.query(queryString, [req.body.feeling, req.body.understanding, req.body.support, req.body.comments])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error POST /feedback', error);
+        res.sendStatus(500);  
+    });//end query
+})//end POST
+
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,17 +3,35 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom';
 
 class Review extends Component {
+
+    submitFeedback = () => {
+        const reduce = this.props.reduxState;
+
+        const postObject = {
+            feeling: reduce.feelingReducer,
+            understanding: reduce.understandingReducer,
+            support: reduce.supportReducer,
+            comments: reduce.commentReducer,
+        }//end postObject
+
+        console.log(postObject)
+    }
+    
+
     render() {
+   
       return (
           <div>
               <h3>Review your feedback:</h3>
-              <ul>
+              <ul className = 'reviewList'>
                     <li>Feelings: {this.props.reduxState.feelingReducer}</li>
-                    <li>Support: {this.props.reduxState.supportReducer}</li> 
                     <li>Understanding {this.props.reduxState.understandingReducer}</li>
+                    <li>Support: {this.props.reduxState.supportReducer}</li> 
                     <li>Comments: {this.props.reduxState.commentReducer}</li>
               </ul>
-              <Link to="/success"><button>Submit Feedback</button></Link>
+              <Link to="/success">
+                  <button onClick = {this.submitFeedback}>Submit Feedback</button>
+                </Link>
           </div>
         );//end return
     }//end render
